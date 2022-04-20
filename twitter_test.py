f6fbd96915fc33363a6760ae71001090adf50379
asim-altayb <asomy0909@gmail.com>
2022-04-20 23:44:43 +0200

    twiter test

---
+++ twitter_test.py
@@ -0,0 +1,66 @@
+import os,time,traceback
+from pymongo import MongoClient
+ 
+start_t = time.time()
+TOTAL_FILES = 0
+INSERTED_FILES = 0
+INSERTED_ROWS = 0
+
+def split_line(txt):
+    delims = [':']
+    for d in delims:
+        result = txt.strip().split(d,maxsplit=1)
+        if len(result) >= 2:
+            return result
+
+    return [txt] # If nothing worked, return the input
+
+def inserter(pathes,P_ID):
+    global TOTAL_FILES
+    global INSERTED_FILES
+    global INSERTED_ROWS
+    for file_path in pathes:
+        input_file = open(file_path,"r")
+        current_batch = []
+        insert_s_time = time.time()
+        with open(file_path,"r") as input_file:
+            try:
+                print("\n start file "+file_path+" =>" + str(P_ID))
+                lines = input_file.read().splitlines()
+                for line in lines: 
+                    try:
+                        split = split_line(line)
+                        if(len(split) >=2):
+                            current_batch.append({"email":split[0], "password":split[1], "source":file_path})
+                            INSERTED_ROWS +=1
+                        else:
+                            print(split)
+                    except Exception:
+                        print(traceback.format_exc())
+                        print(split)
+                print(current_batch[1:100])
+            except Exception:
+                print(traceback.format_exc())
+                print('** File'+file_path+' failed to insert => skip')
+        
+
+def path_splitter():
+    global TOTAL_FILES
+    reader_path = '/home/nawaf/nawafmhm/YouNow_RF/Data'
+    pathes = []
+    for path, currentDirectory, files in os.walk(reader_path):
+        for file in files:
+            if file.endswith(".txt"):
+                TOTAL_FILES +=1
+                start_t = time.time()
+                pathes.append(os.path.join(path, file))
+    return pathes
+
+def main():
+    pathes = path_splitter()
+    inserter(pathes,1)
+    print ("\n Time Taken: %.3f sec" % (time.time()-start_t))
+
+
+if __name__ == '__main__':
+    main()
